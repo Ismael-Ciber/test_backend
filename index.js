774deb88e3c01b14f5da550128e80d1a3162b898
ismael <wanchito0404@gmail.com>
2025-01-23 16:53:05 +0100

    Primer commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const db = require('./initdb');
+const geoip = require('geoip-lite');
+const PORT = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hola mundo");
+});
+
+app.get("/imagenes", (req, res) => {
+    const ip = req.ip;
+    const userAgent = req.get('User-Agent');
+    const fecha = new Date().toISOString();
+    const localización = geoip.lookup(ip);
+    const insert = db.prepare("INSERT INTO usuarios (ip, userAgent, localización, fecha) VALUES (?, ?, ?, ?)");
+    insert.run(ip, userAgent, localización, fecha);
+
+    const imagenes = [
+        "1.jpeg",
+        "2.jpeg",
+        "3.jpeg",
+        "4.jpeg",
+        "5.jpeg"
+    ]
+    const numeroAleatorio = Math.floor(Math.random() * imagenes.length);
+    const imagen = imagenes[numeroAleatorio];
+
+
+    res.sendFile(__dirname + `/${imagen}`)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
